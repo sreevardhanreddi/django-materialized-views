4fd9a71f1013c8499a86df73ee334ab5001a6bde
sreevardhanreddi <sreevardhanreddi@gmail.com>
2021-08-11 16:13:40 +0530

    added views and templates for blogs list pages

--- app/pg_app/urls.py
+++ app/pg_app/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 from pg_app.views import (
     index,
+    blogs,
+    blogs_with_categories,
+    blogs_with_categories_and_tags,
 )
 
 
 app_name = "app"
 
 urlpatterns = [
-    path("", index, name="dashboard"),
+    path("", index, name="index"),
+    path("blogs/", blogs, name="blogs"),
+    path("blogs-with-categories/", blogs_with_categories, name="blogs_with_categories"),
+    path(
+        "blogs-with-categories-and-tags/",
+        blogs_with_categories_and_tags,
+        name="blogs_with_categories_and_tags",
+    ),
 ]
--- app/pg_app/views.py
+++ app/pg_app/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render, HttpResponse
+from pg_app.models import Blog
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    return render(request, "index.html")
+
+
+def blogs(request):
+    blogs = Blog.objects.all()
+    return render(request, "blogs.html", {"blogs": blogs})
+
+
+def blogs_with_categories(request):
+    blogs = Blog.objects.select_related("category").all()
+    # blogs = Blog.objects.all()
+    return render(request, "blogs_with_categories.html", {"blogs": blogs})
+
+
+def blogs_with_categories_and_tags(request):
+    blogs = Blog.objects.select_related("category").prefetch_related("tags").all()
+    # blogs = Blog.objects.all()
+    return render(request, "blogs_with_categories_and_tags.html", {"blogs": blogs})
